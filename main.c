69b164eb4673cf11f13e78744e479b4c46220875
Brandon Dixon <brandon.dixon@sevun.com>
2015-08-15 15:36:42 -0500

    setup basic serial comms

--- main.c
+++ main.c
@@ -1,14 +1,75 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include "inc/hw_gpio.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
 
-/*
- * main.c
- */
-int main(void) {
-	
-	return 0;
+//*****************************************************************************
+// Main code starts here
+//*****************************************************************************
+int main(void)
+{
+	// Enable Peripheral Clocks
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART2);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+
+    // Enable pin PA0 for UART0 U0RX
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0);
+
+    // Enable pin PA1 for UART0 U0TX
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_1);
+
+    // Enable pin PD6 for UART2 U2RX
+    //
+    GPIOPinConfigure(GPIO_PD6_U2RX);
+    GPIOPinTypeUART(GPIO_PORTD_BASE, GPIO_PIN_6);
+
+    // Enable pin PD7 for UART2 U2TX
+    // First open the lock and select the bits we want to modify in the GPIO commit register.
+    HWREG(GPIO_PORTD_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTD_BASE + GPIO_O_CR) = 0x80;
+
+    // Now modify the configuration of the pins that we unlocked.
+    GPIOPinConfigure(GPIO_PD7_U2TX);
+    GPIOPinTypeUART(GPIO_PORTD_BASE, GPIO_PIN_7);
+
+    // Set the clocking to run directly from the crystal.
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+	// Use the internal 16MHz oscillator as the UART clock source.
+	UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+	// Initialize  UART0 using uartstdio
+    UARTStdioConfig(0, 115200, 16000000);
+
+    // Initialize UART2
+	UARTConfigSetExpClk(UART2_BASE, SysCtlClockGet(), 115200,
+			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+//    UARTFIFOEnable(UART2_BASE);
+//    UARTFIFOLevelSet(UART2_BASE, UART_FIFO_TX6_8, UART_FIFO_RX6_8);
+
+	// Clear and reset home screen
+	UARTprintf("\033[2J\033[;H");
+	UARTprintf("Counterwound Labs, Inc.");
+	UARTprintf("\r\nSerial test.");
+	UARTprintf("\r\nHeartbeat Counter = 0x");
+
+	uint32_t ui32Counter = 0;
+
+	while(1)
+	{
+		UARTprintf("\033[3;23f");
+		UARTprintf("%08x",ui32Counter);
+		SysCtlDelay(SysCtlClockGet()/3);	// Delay 1 second
+		ui32Counter++;
+	}
 }
