4ffb224757e7a60646929e5d1d1d2327a9646f5a
Brandon Dixon <brandon.dixon@sevun.com>
2015-10-30 00:21:22 -0500

    added output

--- main.c
+++ main.c
@@ -23,6 +23,7 @@
 
 #define SCREEN_BAUD 	115200
 #define UART_BAUD 		115200
+#define szBufUart 		8
 
 //*****************************************************************************
 // Global Variables
@@ -354,6 +355,50 @@ int main(void)
 	g_sUARTMsgObject7Rx.ui32MsgLen = sizeof(g_pui8MsgTx);
 
 	DrawScreen();
+
+	// data for test messages
+	tMsgObject testMsg1;
+	uint8_t testBuf1[] = {0,1,2,3,4,5,6,7};
+	tMsgObject testMsg2;
+	uint8_t testBuf2[] = {8,9,10,11,12,13,14,15};
+
+	// data for test buffer
+	tMsgObject msgFifoUart[szBufUart];
+	tMsgObject poppedMsg;
+	tMsgBuffer msgBufUart;
+	uint64_t bufCnt;
+	bool bufEmpty;
+
+	// test populating two messages
+	populateMsgObject(&testMsg1, 0xDEADBEEF, testBuf1, 8);
+	populateMsgObject(&testMsg2, 0x600DF00D, testBuf2, 8);
+
+	// initialize various buffer variables
+	initMsgBuffer(&msgBufUart, msgFifoUart, szBufUart);
+
+	// check if empty (should be) and count (should == 0)
+	bufEmpty = isBufEmpty(&msgBufUart);
+	UARTprintf("IsEmpty 0x%x\r\n",bufEmpty);
+	bufCnt = getBufCount(&msgBufUart);
+	UARTprintf("Count 0x%x\r\n",&bufCnt);
+
+	// then push those messages into the buffer
+	pushMsgToBuf(testMsg1, &msgBufUart);
+	pushMsgToBuf(testMsg2, &msgBufUart);
+
+	// check if empty (should not be) and count (should == 2)
+	bufEmpty = isBufEmpty(&msgBufUart);
+	UARTprintf("IsEmpty 0x%x\r\n",bufEmpty);
+	bufCnt = getBufCount(&msgBufUart);
+	UARTprintf("Count 0x%x\r\n",&bufCnt);
+
+	// then pop the first one
+	poppedMsg = popMsgFromBuf(&msgBufUart);
+	UARTprintf("Popped 0x%x\r\n",poppedMsg);
+
+	// then check count (should == 1)
+	bufCnt = getBufCount(&msgBufUart);
+	UARTprintf("Count 0x%x\r\n",&bufCnt);
 
 	while (1)
 	{
